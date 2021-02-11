f9142d8791f79dba79b16a90e49fb25a95a28f95
Mazen Ahmed <mazenahmed96@gmail.com>
2021-02-11 21:18:24 +0200

    Add files via upload
    init

---
+++ whois.sh
@@ -0,0 +1,14 @@
+touch results.txt
+:> results.txt
+
+cat hosts.txt | while read line; 
+do
+    if [ ! -z "$line" ];  then
+        echo "Querying $line" 
+        res=$(whois $line | grep -i "OrgName\|Registrant Organization\|Timeout" | tail -1)
+        if [ -z "$res" ]; then
+            res="Something's wrong"
+        fi
+        echo $line " " $res >> results.txt
+    fi
+done
